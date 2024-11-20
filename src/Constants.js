def63909c83f1f511984d64ec3b239e742b3ece7
ha-kuku <hakukudh@gmail.com>
2024-11-20 13:19:31 +0900

    feat: 메시지 상수화

---
+++ src/Constants.js
@@ -0,0 +1,11 @@
+const INPUT_MESSAGE = Object.freeze({
+  carName: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n',
+  playCount: '시도할 횟수는 몇 회인가요?\n',
+});
+
+const OUTPUT_MESSAGE = Object.freeze({
+  resultAnnounce: '실행 결과',
+  finalWinner: '최종 우승자 : ',
+});
+
+export { INPUT_MESSAGE, OUTPUT_MESSAGE };
