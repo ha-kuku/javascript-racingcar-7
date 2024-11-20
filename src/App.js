49d7f81f436a06a9afdee51fa6ae6e0628f03fcc
ha-kuku <hakukudh@gmail.com>
2024-11-20 13:27:19 +0900

    feat: 자동차 이름 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { INPUT_MESSAGE } from './Constants.js';
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  getCarName = async () => {
+    const input = await Console.readLineAsync(INPUT_MESSAGE.carName);
+  };
+
+  async run() {
+    try {
+      await this.getCarName();
+    } catch (err) {
+      Console.print(err.message);
+      throw err;
+    }
+  }
 }
 
 export default App;
